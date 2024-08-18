dae42b7a40fbd40a169bc34f63980f713c301986
Daikaigan <bravenhebl@gmail.com>
2024-08-18 20:35:31 +0800

    Updated Health HUD and Finalized Menus

--- Scripts/HUD/options.gd
+++ Scripts/HUD/options.gd
@@ -1,10 +1,8 @@
 extends CanvasLayer
 
-#TODO: Add back button functionality
 #TODO: Add sound effect to SFX slider
 
 signal back_button
 
 func _on_back_pressed():
 	back_button.emit()
-	print("pressed")
